e1e8655510636c5e9375a596c37f9cc37cfbdcd2
muunioi <muunii06@gmail.com>
2022-06-30 23:47:50 +0900

    Feat: Create project

---
+++ src/main/java/com/weave/weaveserver/WeaveServerApplication.java
@@ -0,0 +1,13 @@
+package com.weave.weaveserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaveServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaveServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weave/weaveserver/WeaveServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weave.weaveserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaveServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
