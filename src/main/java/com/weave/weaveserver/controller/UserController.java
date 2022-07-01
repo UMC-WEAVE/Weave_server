d91232823d68b55ed3ee045db1303a4a036801c5
hweyoung <gnldud0516@gmail.com>
2022-07-02 00:07:35 +0900

    [Feat] domain build

---
+++ src/main/java/com/weave/weaveserver/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.weave.weaveserver.controller;
+
+import com.weave.weaveserver.dto.JsonResponse;
+import com.weave.weaveserver.dto.UserRequest;
+import com.weave.weaveserver.dto.UserResponse;
+import com.weave.weaveserver.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<?> joinUser(@RequestBody UserRequest.join joinUser){
+        userService.joinUser(joinUser);
+        return ResponseEntity.ok(new JsonResponse(200, "joinUser",null));
+    }
+
+
+
+}
---
+++ src/main/java/com/weave/weaveserver/dto/UserResponse.java
@@ -0,0 +1,10 @@
+package com.weave.weaveserver.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class UserResponse {
+
+}
