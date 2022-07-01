9b7773a0c06e55bba543272cda608046418c72b8
hweyoung <gnldud0516@gmail.com>
2022-07-02 02:32:57 +0900

    Feat : build domain

---
+++ src/main/java/com/weave/weaveserver/domain/Plan.java
@@ -0,0 +1,42 @@
+package com.weave.weaveserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@Entity
+@Table(name = "plan")
+public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int planIdx;
+    private int teamIdx;
+
+    @ManyToOne
+    @JoinColumn(name = "recent_user_idx")
+    private User user;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime date;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private String location;
+
+    private float latitude;
+
+    private float longtitude;
+
+    private int cost;
+}
