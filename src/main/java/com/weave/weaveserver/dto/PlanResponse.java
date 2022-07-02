44679046614bfdf1f88f6da705368b02d90e6756
lorlorv <dev.eunseo@gmail.com>
2022-07-02 17:09:55 +0900

    Refactor : getPlanList refactoring

--- src/main/java/com/weave/weaveserver/dto/PlanResponse.java
+++ src/main/java/com/weave/weaveserver/dto/PlanResponse.java
@@ -12,39 +12,15 @@ public class PlanResponse {
     @AllArgsConstructor
     @Data
     public static class planRes{
-        private int teamIdx;
-        private LocalDateTime date;
-        private String title;
-        private LocalDateTime start_time;
-        private LocalDateTime end_time;
-        private String location;
-        private int userIdx;
-    }
-
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Data
-    public static class planListRes{
-
         private int planIdx;
         private int teamIdx;
-        private int userIdx;
-        private String title;
         private LocalDateTime date;
+        private String title;
         private LocalDateTime start_time;
         private LocalDateTime end_time;
         private String location;
-        private float latitude;
-        private float longitude;
         private int cost;
-
-        public planListRes(int planIdx, int teamIdx, int userIdx, String title, LocalDateTime date) {
-            this.planIdx = planIdx;
-            this.teamIdx = teamIdx;
-            this.userIdx = userIdx;
-            this.title = title;
-            this.date = date;
-        }
+        private int userIdx;
     }
 
 }
