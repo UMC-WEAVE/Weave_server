aa4bf821bd5cf5deac817dce8af2c5e7ff35fbc8
jeeheaG <jeehea99@naver.com>
2022-08-12 12:30:31 +0900

    Feat : Modify Archive APIs by using ResponseEntity<Object> instead of using wildcard like ResponseEntity<?>

--- src/main/java/com/weave/weaveserver/controller/ArchiveController.java
+++ src/main/java/com/weave/weaveserver/controller/ArchiveController.java
@@ -16,32 +16,32 @@ import java.util.List;
 public class ArchiveController {
     private final ArchiveService archiveService;
 
-    @PostMapping("/archives") // TODO : 이후 ResponseEntity<Object> 로 수정해볼 것
-    public ResponseEntity<?> createArchive(@RequestBody ArchiveRequest.createRequest request, HttpServletRequest servletRequest){ //RequestBody 에 입력값들을 담고, Header 에 유저의 토큰을 담아 보냄.
+    @PostMapping("/archives")
+    public ResponseEntity<Object> createArchive(@RequestBody ArchiveRequest.createRequest request, HttpServletRequest servletRequest){ //RequestBody 에 입력값들을 담고, Header 에 유저의 토큰을 담아 보냄.
         archiveService.addArchive(request, servletRequest);
         return ResponseEntity.ok(new JsonResponse(200, "success addArchive", null));
     }
 
     @GetMapping("/teams/{teamIdx}/archives")
-    public ResponseEntity<?> getArchiveList(@PathVariable Long teamIdx){
+    public ResponseEntity<Object> getArchiveList(@PathVariable Long teamIdx){
         ArchiveResponse.archiveListResponseContainer archiveListContainer = archiveService.getArchiveList(teamIdx);
         return ResponseEntity.ok(new JsonResponse(200, "success getArchiveList", archiveListContainer));
     }
 
     @GetMapping("/archives/{archiveIdx}")
-    public ResponseEntity<?> getArchiveDetail(@PathVariable Long archiveIdx){
+    public ResponseEntity<Object> getArchiveDetail(@PathVariable Long archiveIdx){
         ArchiveResponse.archiveResponse archiveDetail = archiveService.getArchiveDetail(archiveIdx);
         return ResponseEntity.ok(new JsonResponse(200, "success getArchiveDetail", archiveDetail));
     }
 
     @PatchMapping("/archives/{archiveIdx}/pin")
-    public ResponseEntity<?> updateArchivePin(@PathVariable Long archiveIdx){
+    public ResponseEntity<Object> updateArchivePin(@PathVariable Long archiveIdx){
         archiveService.updateArchivePin(archiveIdx);
         return ResponseEntity.ok(new JsonResponse(200, "success updateArchivePin", null));
     }
 
     @DeleteMapping("/archives/{archiveIdx}")
-    public ResponseEntity<?> deleteArchive(@PathVariable Long archiveIdx){
+    public ResponseEntity<Object> deleteArchive(@PathVariable Long archiveIdx){
         archiveService.deleteArchive(archiveIdx);
         return ResponseEntity.ok(new JsonResponse(200, "success deleteArchive", null));
     }
