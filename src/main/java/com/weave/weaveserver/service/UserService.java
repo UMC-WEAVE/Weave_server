dfac88aeeff06e37ccd816640f0fecce9baae04b
hweyoung <gnldud0516@gmail.com>
2022-09-18 00:57:32 +0900

    Refactor : refactoring userService

--- src/main/java/com/weave/weaveserver/service/UserService.java
+++ src/main/java/com/weave/weaveserver/service/UserService.java
@@ -23,10 +23,6 @@ public class UserService {
     private final UserRepository userRepository;
     private final BelongRepository belongRepository;
 
-    private final TeamRepository teamRepository;
-    private final PlanRepository planRepository;
-    private final ArchiveRepository archiveRepository;
-
     private final PlanService planService;
 
     @Transactional
@@ -52,90 +48,29 @@ public class UserService {
     }
 
 
-    @Transactional
-    public void deleteUser(String email) {
-        //TODO : test 12번 유저
-        try {
-            User user = userRepository.findUserByEmail(email);
-        }catch (NullPointerException e){
-            throw new BadRequestException("이미 삭제된 유저");
-        }
-        User user = userRepository.findUserByEmail(email);
-        if(user==null){
-            throw new BadRequestException("존재하지 않는 token입니다.");
-        }
-        try{
-            deleteTeamByLeaderIdx(user.getUserIdx());
-            deletePlanByUserIdx(user.getUserIdx());
-            deleteArchiveByUserIdx(user.getUserIdx());
-
-        }catch (BadRequestException e){
-            System.out.println("등록되지 않은 팀이래요~~");
-        }
-        userRepository.delete(user);
-        System.out.println(user.getUserIdx()+"번째 유저 삭제 완료");
-    }
-
-    //team 삭제시 plan, archive 전부 삭제 => teamIdx
-    @Transactional
-    public void deleteTeamByLeaderIdx(Long userIdx){
-        List<Team> teamList = teamRepository.findALLByLeaderIdx(userIdx).orElseThrow(()->new BadRequestException("team이 등록되어있지 않은 user"));
-        for(Team team : teamList){
-            System.out.println("deleteTeam : "+team.getTeamIdx());
-            deleteArchiveByTeamIdx(team.getTeamIdx());
-            deletePlanByTeamIdx(team.getTeamIdx());
-            teamRepository.delete(team);
-        }
-        System.out.println("팀삭제 끝!");
-    }
-
-    //user 탈퇴시 -> plan 삭제
-    //team 삭제시 -> plan 삭제
-    @Transactional
-    public void deletePlanByUserIdx(Long userIdx){
-        planRepository.deleteAllByUserIdx(userIdx);
-//        List<Plan> planList = planRepository.findALLByUserIdx(userIdx).orElseThrow(()->new BadRequestException("plan이 등록되어있지 않은 user"));
-//        for(Plan plan : planList){
-//            System.out.println("deletePlan : "+plan.getPlanIdx());
-//            planRepository.delete(plan);
+//    @Transactional
+//    public void deleteUser(String email) {
+//        //TODO : test 12번 유저
+//        try {
+//            User user = userRepository.findUserByEmail(email);
+//        }catch (NullPointerException e){
+//            throw new BadRequestException("이미 삭제된 유저");
 //        }
-//        System.out.println("플랜 삭제 끝!");
-    }
-
-    @Transactional
-    public void deletePlanByTeamIdx(Long teamIdx){
-        List<Plan> planList = planRepository.findALLByTeamIdx(teamIdx).orElseThrow(()->new BadRequestException("plan이 등록되어있지 않은 user"));
-        for(Plan plan : planList){
-            System.out.println("deletePlan : "+plan.getPlanIdx());
-            planRepository.delete(plan);
-        }
-        System.out.println("플랜 삭제 끝!");
-    }
-
-    //user 탈퇴시 -> archive (user = null)
-    //team 삭제시 -> archive 삭제
-    @Transactional
-    public void deleteArchiveByUserIdx(Long userIdx){
-        List<Archive> archiveList = archiveRepository.findAllByUserIdx(userIdx).orElseThrow(()->new BadRequestException("archive가 등록되어있지 않은 user"));
-        for(Archive archive : archiveList){
-            System.out.println("deleteArchive : "+archive.getArchiveIdx());
-            archiveRepository.delete(archive);
-//            System.out.println("updateArchive => user null : "+archive.getArchiveIdx());
-//            archive.setUser(null);
-//            archiveRepository.save(archive);
-        }
-        System.out.println("아카이브 삭제 끝!");
-    }
-
-    @Transactional
-    public void deleteArchiveByTeamIdx(Long teamIdx){
-        List<Archive> archiveList = archiveRepository.findAllByTeamIdx(teamIdx).orElseThrow(()->new BadRequestException("archive가 등록되어있지 않은 user"));
-        for(Archive archive : archiveList){
-            System.out.println("deleteArchive : "+archive.getArchiveIdx());
-            archiveRepository.delete(archive);
-        }
-        System.out.println("아카이브 삭제 끝!");
-    }
+//        User user = userRepository.findUserByEmail(email);
+//        if(user==null){
+//            throw new BadRequestException("존재하지 않는 token입니다.");
+//        }
+//        try{
+//            deleteTeamByLeaderIdx(user.getUserIdx());
+//            deletePlanByUserIdx(user.getUserIdx());
+//            deleteArchiveByUserIdx(user.getUserIdx());
+//
+//        }catch (BadRequestException e){
+//            System.out.println("등록되지 않은 팀이래요~~");
+//        }
+//        userRepository.delete(user);
+//        System.out.println(user.getUserIdx()+"번째 유저 삭제 완료");
+//    }
 
 
 }
