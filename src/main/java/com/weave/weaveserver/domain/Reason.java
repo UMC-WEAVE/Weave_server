1fac66318ca41569e94c7e74cebbdb4dec4c2c1e
lorlorv <dev.eunseo@gmail.com>
2022-10-23 23:20:28 +0900

    Feat : Member Quit Reason Entity

---
+++ src/main/java/com/weave/weaveserver/domain/Reason.java
@@ -0,0 +1,34 @@
+package com.weave.weaveserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Reason extends BaseTime{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "ReasonSequence")
+    @Column(unique=true)
+    private Long reasonIdx;
+
+    @Column
+    private boolean item1;
+
+    @Column
+    private boolean item2;
+
+    @Column
+    private boolean item3;
+
+    @Column(length = 300)
+    private String sub_item;
+}
